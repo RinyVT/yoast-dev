5895d2c37762e5fdca0a9e1555c8726eb90b2f89
Riny van Tiggelen <info@online-gamer.nl>
2021-07-02 22:16:41 +0200

    [TASK] Added fluid styled content typoscript to sitepackage

--- extensions/sitepackage/ext_localconf.php
+++ extensions/sitepackage/ext_localconf.php
@@ -2,6 +2,9 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(static function ($extKey) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(
+        "@import 'EXT:sitepackage/Configuration/TypoScript/constants.typoscript'"
+    );
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         "@import 'EXT:sitepackage/Configuration/TypoScript/setup.typoscript'"
     );
